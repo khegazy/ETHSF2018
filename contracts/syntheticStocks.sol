67027c940bf06dad20959cd617ff0f1b2d63fb9d
khegazy <kareemhegz@gmail.com>
2018-10-07 07:11:16 -0700

    Added contract query functions and functionality to end the agreement if certain conditions are met.

--- contracts/syntheticStocks.sol
+++ contracts/syntheticStocks.sol
@@ -18,17 +18,20 @@ contract syntheticStocks {
   }
 
   struct agreement {
+    bool    isValid;
     address client;
     address provider;
 
     uint256 providerCollateralType;
     uint256 providerCollateralAmount;
 
-    uint256 ClientCollateralType;
-    uint256 ClientCollateralAmount;
+    uint256 clientCollateralType;
+    uint256 clientCollateralAmount;
 
     bytes32 stock;
     uint256 startPrice;
+    uint256 endPrice;
+    uint256 buffer;
 
     uint256 endBlock;
   }
@@ -61,6 +64,7 @@ contract syntheticStocks {
   mapping (uint256 => tempAgreement) private _allTempAgreements;
 
   uint256 currentAgrIndex;
+  uint256[] _validAgreements;
   mapping (uint256 => agreement) _allAgreements;
   mapping (address => uint256[]) _agreements_clients;
   mapping (address => uint256[]) _agreements_providers;
@@ -192,7 +196,7 @@ contract syntheticStocks {
      Function called by provider to offer an agreement
      to the client.
      */
-  function newAgreement (
+  function createOpenAgreement (
       uint256 index,
       uint256 collateralType,
       uint256 collateralAmount) public payable returns(bool succuss) {
@@ -225,12 +229,60 @@ contract syntheticStocks {
     return true;
   }
 
+  /*
+     Get open agreements.
+     */
+  function getOpenAgreements_clients() public view returns (
+      bytes32[], 
+      //bytes32[], 
+      uint256[], 
+      uint256[]) {
+
+    bytes32[] memory stocks  = new bytes32[](_tempAgreements_clients[msg.sender].length);
+    //bytes32[] memory providers = new bytes32[](_tempAgreements_clients[msg.sender].length);
+    uint256[] memory collateral = new uint256[](_tempAgreements_clients[msg.sender].length);
+    uint256[] memory endBlock   = new uint256[](_tempAgreements_clients[msg.sender].length);
+
+    for (uint i=0; i<_tempAgreements_clients[msg.sender].length; i++) {
+      stocks[i]  = _allTempAgreements[_tempAgreements_clients[msg.sender][i]].stock;
+      //providers[i] = _allOffers[_tempAgreements_clients[msg.sender][i]].client;
+      collateral[i] = _allTempAgreements[_tempAgreements_clients[msg.sender][i]].providerCollateralAmount;
+      endBlock[i]   = _allTempAgreements[_tempAgreements_clients[msg.sender][i]].endBlock;
+    }
+
+    //return (stocks, clients, collateral, endBlock);
+    return (stocks, collateral, endBlock);
+  }
+
+  
+  function getOpenAgreements_providers() public view returns (
+      bytes32[], 
+      //bytes32[], 
+      uint256[], 
+      uint256[]) {
+
+    bytes32[] memory stocks  = new bytes32[](_tempAgreements_providers[msg.sender].length);
+    //bytes32[] memory clients = new bytes32[](_tempAgreements_providers[msg.sender].length);
+    uint256[] memory collateral = new uint256[](_tempAgreements_providers[msg.sender].length);
+    uint256[] memory endBlock   = new uint256[](_tempAgreements_providers[msg.sender].length);
+
+    for (uint i=0; i<_tempAgreements_providers[msg.sender].length; i++) {
+      stocks[i]  = _allTempAgreements[_tempAgreements_providers[msg.sender][i]].stock;
+      //clients[i] = _allOffers[_tempAgreements_clients[msg.sender][i]].client;
+      collateral[i] = _allTempAgreements[_tempAgreements_providers[msg.sender][i]].clientCollateralAmount;
+      endBlock[i]   = _allTempAgreements[_tempAgreements_providers[msg.sender][i]].endBlock;
+    }
+
+    //return (stocks, clients, collateral, endBlock);
+    return (stocks, collateral, endBlock);
+  }
+
 
   /*
      Client rejects agreement from provider.
      */
   function rejectAgreement(uint256 index) public {
-    removeTempAgreement(index);
+    removeOpenAgreement(index);
   }
 
 
@@ -242,33 +294,87 @@ contract syntheticStocks {
 
     agreement memory agr;
 
-    //bytes32 memory btsStock = _allTempAgreements[index].stock;
-    string memory strStock = bytes32ToStr(_allTempAgreements[index].stock); //string(btsStock);
+    string memory strStock = bytes32ToStr(_allTempAgreements[index].stock);
     getPrice(strStock); 
 
+    agr.isValid     = true;
     agr.stock       = _allTempAgreements[index].stock;
     agr.endBlock    = _allTempAgreements[index].endBlock;
-    agr.startPrice  = currentPrice; //TODO: GET PRICE
+    agr.startPrice  = currentPrice; 
+    agr.endPrice    = 0; 
+    agr.buffer      = 10000;
     agr.providerCollateralType     =  _allTempAgreements[index].providerCollateralType;
     agr.providerCollateralAmount   =  _allTempAgreements[index].providerCollateralAmount;
-    agr.providerCollateralType   =  _allTempAgreements[index].providerCollateralType;
-    agr.providerCollateralAmount =  _allTempAgreements[index].providerCollateralAmount;
+    agr.clientCollateralType       =  _allTempAgreements[index].clientCollateralType;
+    agr.clientCollateralAmount     =  _allTempAgreements[index].clientCollateralAmount;
 
     _agreements_clients[msg.sender].push(currentAgrIndex);
     _agreements_providers[_allTempAgreements[index].provider].push(currentAgrIndex);
 
     _allAgreements[currentAgrIndex] = agr;
+    _validAgreements.push(currentAgrIndex);
     currentAgrIndex++;
 
     removeOffer(_allTempAgreements[index].offerIndex);
-    removeTempAgreement(index);
+    removeOpenAgreement(index);
+  }
+
+  function getAgreements_clients() public view returns (
+      bytes32[], 
+      //bytes32[], 
+      uint256[], 
+      uint256[], 
+      uint256[]) {
+
+    bytes32[] memory stocks  = new bytes32[](_agreements_clients[msg.sender].length);
+    //bytes32[] memory providers = new bytes32[](_agreements_clients[msg.sender].length);
+    uint256[] memory collateral_providers = new uint256[](_agreements_clients[msg.sender].length);
+    uint256[] memory collateral_clients = new uint256[](_agreements_clients[msg.sender].length);
+    uint256[] memory endBlock   = new uint256[](_agreements_clients[msg.sender].length);
+
+    for (uint i=0; i<_agreements_clients[msg.sender].length; i++) {
+      stocks[i]  = _allAgreements[_agreements_clients[msg.sender][i]].stock;
+      //providers[i] = _allAgreements[_agreements_clients[msg.sender][i]].provider;
+      collateral_providers[i] = _allAgreements[_agreements_clients[msg.sender][i]].providerCollateralAmount;
+      collateral_clients[i] = _allAgreements[_agreements_clients[msg.sender][i]].clientCollateralAmount;
+      endBlock[i]   = _allAgreements[_agreements_clients[msg.sender][i]].endBlock;
+    }
+
+    //return (stocks, clients, collateral, endBlock);
+    return (stocks, collateral_clients, collateral_providers, endBlock);
+  }
+
+
+  function getAgreements_providers() public view returns (
+      bytes32[], 
+      //bytes32[], 
+      uint256[], 
+      uint256[], 
+      uint256[]) {
+
+    bytes32[] memory stocks  = new bytes32[](_agreements_providers[msg.sender].length);
+    //bytes32[] memory clients = new bytes32[](_agreements_providers[msg.sender].length);
+    uint256[] memory collateral_clients = new uint256[](_agreements_providers[msg.sender].length);
+    uint256[] memory collateral_providers = new uint256[](_agreements_providers[msg.sender].length);
+    uint256[] memory endBlock   = new uint256[](_agreements_providers[msg.sender].length);
+
+    for (uint i=0; i<_agreements_providers[msg.sender].length; i++) {
+      stocks[i]  = _allAgreements[_agreements_providers[msg.sender][i]].stock;
+      //clients[i] = _allAgreements[_agreements_providers[msg.sender][i]].client;
+      collateral_clients[i] = _allAgreements[_agreements_providers[msg.sender][i]].clientCollateralAmount;
+      collateral_providers[i] = _allAgreements[_agreements_providers[msg.sender][i]].providerCollateralAmount;
+      endBlock[i]   = _allAgreements[_agreements_providers[msg.sender][i]].endBlock;
+    }
+
+    //return (stocks, clients, collateral, endBlock);
+    return (stocks, collateral_clients, collateral_providers, endBlock);
   }
 
 
   /*
      Remove temporary agreement either buy revoking or rejecting it.
      */
-  function removeTempAgreement(uint256 index) public {
+  function removeOpenAgreement(uint256 index) public {
 
     require((msg.sender == _allTempAgreements[index].client)
         || (msg.sender == _allTempAgreements[index].provider));
@@ -312,4 +418,69 @@ contract syntheticStocks {
     delete _allTempAgreements[index];
   }
 
+
+  /*
+     Check collateral conditions
+     */
+  function endAgreements() public {
+    bool endAgreement = false;
+    uint256 startPrice;
+    uint256 ethConversion = 1;
+    for (uint i=0; i<_validAgreements.length; i++) {
+      if (block.number > _allAgreements[_validAgreements[i]].endBlock) {
+        endAgreement = true;
+      }
+
+      startPrice = _allAgreements[_validAgreements[i]].startPrice;
+      string memory strStock = bytes32ToStr(_allTempAgreements[i].stock);
+      getPrice(strStock);
+      
+
+      if (currentPrice > startPrice) {
+        if (endAgreement || ((currentPrice - startPrice)*ethConversion
+              > 95*_allAgreements[_validAgreements[i]].buffer)) {
+
+           _allAgreements[_validAgreements[i]].client.transfer(
+              _allAgreements[_validAgreements[i]].clientCollateralAmount);
+          uint256 earnedAmount = _allAgreements[_validAgreements[i]].providerCollateralAmount
+              *(currentPrice - startPrice)*ethConversion
+              /(_allAgreements[_validAgreements[i]].providerCollateralAmount);
+          _allAgreements[_validAgreements[i]].client.transfer(earnedAmount);
+          _allAgreements[_validAgreements[i]].provider.transfer(
+              _allAgreements[_validAgreements[i]].providerCollateralAmount - earnedAmount);
+
+          endAgreement = true;
+        }
+      }
+      else {
+        if (endAgreement || ((startPrice - currentPrice)*ethConversion
+              > _allAgreements[_validAgreements[i]].buffer)) {
+
+           _allAgreements[_validAgreements[i]].provider.transfer(
+              _allAgreements[_validAgreements[i]].providerCollateralAmount);
+          uint256 lostAmount = _allAgreements[_validAgreements[i]].clientCollateralAmount
+              *(startPrice - currentPrice)*ethConversion
+              /(_allAgreements[_validAgreements[i]].clientCollateralAmount);
+          _allAgreements[_validAgreements[i]].provider.transfer(lostAmount);
+          _allAgreements[_validAgreements[i]].client.transfer(
+              _allAgreements[_validAgreements[i]].providerCollateralAmount - lostAmount);
+
+          endAgreement = true;
+        }
+      }
+
+      if (endAgreement) {
+        _allAgreements[_validAgreements[i]].isValid = false;
+        _allAgreements[_validAgreements[i]].endPrice = currentPrice;
+
+        for (uint256 j=i+1; j<_validAgreements.length; j++) {
+          _validAgreements[j-1] = _validAgreements[j];
+        }
+        _validAgreements.length--;
+        endAgreement = false;
+      }
+    }
+  }
+
+  
 }
