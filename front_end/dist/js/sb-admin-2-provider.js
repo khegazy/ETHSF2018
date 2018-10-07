26a208914d006b052e7c1ae341565463fc22eb28
Pratyaksh Sharma <pratyaksh@me.com>
2018-10-07 09:18:21 -0700

    jay's front end updates

--- front_end/dist/js/sb-admin-2-provider.js
+++ front_end/dist/js/sb-admin-2-provider.js
@@ -1,17 +1,17 @@
 var USER_BLOOM_ID = 55555;
 var dummy_offers = [
-    ["TSLA", "1.000", "Nov. 5, 2018"],
-    ["GOOG", "5.000", "Nov. 19, 2018"],
-    ["AAPL", "1.430", "Dec. 2, 2018"],
-    ["TSLA", "1.000", "Nov. 5, 2018"],
-    ["GOOG", "5.000", "Nov. 19, 2018"],
-    ["AAPL", "1.430", "Dec. 2, 2018"],
+    ["TSLA",  "22222", "1.000", "Nov. 5, 2018"],
+    ["GOOG",  "11111", "5.000", "Nov. 19, 2018"],
+    ["AAPL",  "33333", "1.430", "Dec. 2, 2018"],
+    ["TSLA",  "44444", "1.000", "Nov. 5, 2018"],
+    ["GOOG",  "77777", "5.000", "Nov. 19, 2018"],
+    ["AAPL",  "66666", "1.430", "Dec. 2, 2018"],
 ];
 var dummy_agreements = [
     ["AMZN", "55555", "11.045", "Nov. 6, 2018"],
     ["SPOT", "22222", "1.200", "Nov. 19, 2018"],
     ["SNAP", "88888", "0.100", "Dec. 2, 2018"],
-    ["AMZN", "55555", "11.045", "Nov. 6, 2018"],
+    ["AMZN", "77777", "11.045", "Nov. 6, 2018"],
     ["SPOT", "22222", "1.200", "Nov. 19, 2018"],
     ["SNAP", "88888", "0.100", "Dec. 2, 2018"],
 ];
@@ -206,33 +206,9 @@ var abi =[
 var address = "0x6e2fc049bd9ff518a9ed26d744d5e747370c93d3";
 
 function accept_buttons(index) {
-    return "<button class='accept_agreement' data-index=" + index + ">accept</button> " +
-        "<button class='reject_agreement' data-index=" + index + ">reject</button>";
+    return "<button class='accept_offer' data-index=" + index + ">accept</button> ";
 };
 
-if (document.getElementById("create_offer") !== null) {
-    document.getElementById("create_offer").onclick = create_offer;
-};
-
-function create_offer() {
-
-    document.getElementById("offer_table").innerHTML +=
-        "<td>" + document.getElementById('fname').value + "</td>" +
-        "<td>" + document.getElementById('fcollateral').value + "</td>" +
-        "<td>" + document.getElementById('fdate').value + "</td>"
-};
-
-if (document.getElementById("create_agreement") !== null) {
-    document.getElementById("create_agreement").onclick = create_agreement;
-};
-
-function create_agreement() {
-    document.getElementById("agreement_table").innerHTML +=
-        "<td>" + document.getElementById('fname').value + "</td>" +
-        "<td>"+ USER_BLOOM_ID+ "</td>" +
-        "<td>" + document.getElementById('fcollateral').value + "</td>" +
-        "<td>" + document.getElementById('fdate').value + "</td>"
-};
 
 function create_row(items) {
     var result = "";
@@ -243,14 +219,21 @@ function create_row(items) {
 };
 
 function load_offers() {
-    dummy_offers.forEach(function(offer_list) {
+    dummy_offers.forEach(function(offer_list, index) {
+        offer_list.push(accept_buttons(index));
+        document.getElementById("offer_table").innerHTML += create_row(offer_list);
+    });
+};
+
+function refresh_offers() {
+    document.getElementById("offer_table").innerHTML = "";
+    dummy_offers.forEach(function(offer_list, index) {
         document.getElementById("offer_table").innerHTML += create_row(offer_list);
     });
 };
 
 function load_agreements() {
     dummy_agreements.forEach(function(agreements_list, index) {
-        agreements_list.push(accept_buttons(index));
         document.getElementById("agreements_table").innerHTML += create_row(agreements_list);
     });
 };
@@ -280,14 +263,16 @@ function get_current_ticker_price(ticker) {
 };
 
 function match_trade(index) {
-    var agreement = dummy_agreements[index];
-    var collateral = get_current_ticker_price(agreement[0]);
-    dummy_contracts.push([agreement[0], agreement[1], collateral, collateral, agreement[2], agreement[3]]);
-    dummy_agreements= dummy_agreements.filter( function(cur_agreement) {
-        return cur_agreement[0] != agreement[0];
-    });
-    refresh_contracts();
+    var offer = dummy_offers[index];
+    dummy_offers.splice(index,1);
+    var collateral = document.getElementById('fcollateral').value ;
+    // dummy_agreements.push([offer[0], collateral, offer[1], collateral, offer[2], offer[3]]);
+    // dummy_agreements= dummy_agreements.filter( function(cur_agreement) {
+    //     return cur_agreement[0] != agreement[0];
+    // });
+    dummy_agreements.push([offer[0], offer[1], collateral, offer[3]]);
     refresh_agreements();
+    refresh_offers();
     setup_buttons();
 };
 
@@ -298,58 +283,49 @@ function reject_trade(index){
 };
 
 function setup_buttons() {
-    var accept_buttons = document.querySelectorAll('.accept_agreement');
-    var reject_buttons = document.querySelectorAll('.reject_agreement');
+    var accept_buttons = document.querySelectorAll('.accept_offer');
+    // var reject_buttons = document.querySelectorAll('.reject_offer');
 
-    var accept_agreement = function(index) {
+    var accept_offer = function(index) {
         return function handle_accept(event) {
             match_trade(index);
         };
     };
 
-    var reject_agreement = function(index) {
-        return function handle_reject(event) {
-            reject_trade(index);
-        };
-    };
-
     for(var i=0; i<accept_buttons.length; i++) {
-        accept_buttons[i].addEventListener("click", accept_agreement(i), false);
+        accept_buttons[i].addEventListener("click", accept_offer(i), false);
     };
 
-    for(var i=0; i<reject_buttons.length; i++) {
-        reject_buttons[i].addEventListener("click", reject_agreement(i), false);
-    };
 };
 
-window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-
-  // console.log("Using web3 version: " + Web3.version);
-  // if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
-  // web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
-  // Get the account of the user from metamask
-
-
-    if (typeof web3 !== 'undefined') {
-      web3 = new Web3(web3.currentProvider);
-    } else {
-      // Set the provider you want from Web3.providers
-      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-
-    const userAccounts = web3.eth.accounts; // resolves on an array of accounts
-    userAccount = userAccounts[0];
-
-    console.log("User account:", userAccount);
-
-    var contract = new web3.eth.contract(abi, address);
-    var ticker = 'AMZN';
-    console.log(contract);
-
-    console.log(contract.newAgreement(ticker, 0, get_current_ticker_price(ticker) * 10**8).call({from: userAccount}));
-    }
-
-});
+// window.addEventListener('load', function() {
+//   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+//
+//   // console.log("Using web3 version: " + Web3.version);
+//   // if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
+//   // web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
+//   // Get the account of the user from metamask
+//
+//
+//     if (typeof web3 !== 'undefined') {
+//       web3 = new Web3(web3.currentProvider);
+//     } else {
+//       // Set the provider you want from Web3.providers
+//       web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+//
+//     const userAccounts = web3.eth.accounts; // resolves on an array of accounts
+//     userAccount = userAccounts[0];
+//
+//     console.log("User account:", userAccount);
+//
+//     var contract = new web3.eth.contract(abi, address);
+//     var ticker = 'AMZN';
+//     console.log(contract);
+//
+//     console.log(contract.newAgreement(ticker, 0, get_current_ticker_price(ticker) * 10**8).call({from: userAccount}));
+//     }
+//
+// });
 
 $(document).ready(function() {
     load_offers();
--- truffle.js
+++ truffle.js
@@ -13,6 +13,18 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0xec3e507b41e20d37e433eb04ce8832055e5b4fa7", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
+  }
 };
